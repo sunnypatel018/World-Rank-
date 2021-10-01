4dd8a8b7ff7492e51cb9c6bd9922fb84fef1e0cc
SUNNY PATEL <isunnypatel272@gmail.com>
2021-10-01 15:15:02 -0400

    filter functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,33 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 import Layout from '../components/Layout/layout';
 import styles from '../styles/Home.module.css';
 import SearchInput from '../components/SearchInput/SearchInput';
 import CountriesTable from '../components/CountriesTable/CountriesTable';
 export default function Home({ countries }) {
+    const [keyword, setKeyword] = useState('');
+
+    const filterCountries = countries.filter((country) =>
+        country.name.toLowerCase().includes(keyword)
+    );
+
+    const onInputChange = (e) => {
+        e.preventDefault();
+        setKeyword(e.target.value.toLowerCase());
+    };
+
     return (
         <Layout>
             <div className={styles.counts}>
                 Found {countries.length} Countries
             </div>
-            <SearchInput placeholder='Filter by Name, Region or Sub Region' />
+            <SearchInput
+                placeholder='Filter by Name, Region or Sub Region'
+                onChange={onInputChange}
+            />
 
-            <CountriesTable countries={countries} />
+            <CountriesTable countries={filterCountries} />
         </Layout>
     );
 }
